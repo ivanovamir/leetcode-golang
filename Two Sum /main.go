456e8c096bb0f066fe24a71306a54d3f162e686c
ivanovamir <adamstradvers@gmail.com>
2024-01-10 19:26:41 +0300

    new problems added

--- Two Sum /main.go
+++ Two Sum /main.go
@@ -19,6 +19,25 @@ func twoSum(nums []int, target int) []int {
 	return nil
 }
 
+// O(2n) = O(n) | O(n)
+//func twoSum(nums []int, target int) []int {
+//	numsMap := make(map[int]int, len(nums))
+//	for i, val := range nums {
+//		numsMap[val] = i
+//	}
+//
+//	for i, val := range nums {
+//		c := target - val
+//		idx, ok := numsMap[c]
+//
+//		if ok && i != idx {
+//			return []int{idx, i}
+//		}
+//	}
+//
+//	return nil
+//}
+
 func main() {
 	fmt.Println(twoSum([]int{2, 7, 11, 15}, 9))
 }
